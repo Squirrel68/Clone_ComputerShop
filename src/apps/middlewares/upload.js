accf792979cb6953e0b5defefdc0fd52854c1637
Squirrel68 <constantlypeacinside@gmail.com>
2023-08-27 22:30:31 +0700

    reset project

--- src/apps/middlewares/upload.js
+++
@@ -1,6 +0,0 @@
-const multer = require("multer");
-const tmpRoute = `${__dirname}/../../tmp`;
-const upload = multer({
-  dest: tmpRoute,
-});
-module.exports = upload;
--- src/bin/www.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv");
-
-dotenv.config({ path: `${__dirname}/../../config.env` });
-const app = require(`${__dirname}/../apps/app.js`);
-
-const port = process.env.PORT || 3000;
-app.listen(port, (req, res) => {
-  console.log(`Server running on port ${port}`);
-});
